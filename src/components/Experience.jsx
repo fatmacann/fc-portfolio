2974d30f34063102bce612356f3ec9703ada0c63
fatmacann <canf261012@gmail.com>
2023-07-16 16:50:48 +0300

    experience page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -81,7 +81,7 @@ const Experience = () => {
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-3 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
