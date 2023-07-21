54c26d066bb49c58c9c5aa33f799fd59b4832527
fatmacann <canf261012@gmail.com>
2023-07-22 00:35:28 +0300

    image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-1/50 md:w-1/3"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
       </div>
